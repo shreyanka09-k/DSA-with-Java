4315be3f34d61d2717ef442a8bcb395ae7686cdc
Shreyanka <yourgmail@gmail.com>
2026-01-30 10:16:46 +0530

    Added Count Even Numbers problem

---
+++ Arrays/CountEven.java
@@ -0,0 +1,21 @@
+public class CountEven {
+
+
+    public static void main(String[] args){
+        int arr[]= {2, 5, 8, 7, 10};
+        //int even=arr[0];
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                 System.out.println(arr[i]);
+
+               //even=arr[i] ;
+            }
+
+        }
+       //// System.out.println(even);
+
+
+
+    }
+    
+}
